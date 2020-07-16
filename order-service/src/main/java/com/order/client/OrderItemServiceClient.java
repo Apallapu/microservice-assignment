10916aeceea62beae1501d2d6b62979ea702ca08
apallapu_java <ankamma.java@gmail.com>
2020-07-16 08:57:34 +0530

    done

--- order-service/src/main/java/com/order/client/OrderItemServiceClient.java
+++ order-service/src/main/java/com/order/client/OrderItemServiceClient.java
@@ -2,11 +2,12 @@ package com.order.client;
 
 import com.order.model.OrderItemDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "ORDER-ITEM-SERVICE")
+@FeignClient(name = "ORDER-ITEM-SERVICE",fallback=OrderItemServiceClient.OrderItemServiceClientFallback.class)
 public interface OrderItemServiceClient {
 
     @PostMapping("/order-item")
@@ -23,6 +24,50 @@ public interface OrderItemServiceClient {
 
     @DeleteMapping("/order-item/{id}")
     void deleteOrderItemById(@PathVariable Long id) ;
+
+    @Component
+    public static class OrderItemServiceClientFallback implements OrderItemServiceClient {
+
+        @Override
+        public OrderItemDTO createOrderItem(OrderItemDTO orderItemDTO) {
+            OrderItemDTO orderItemDTO1=new OrderItemDTO();
+            orderItemDTO1.setId(1l);
+            orderItemDTO1.setQuantity("Good");
+            orderItemDTO1.setProductName("AUDI");
+            orderItemDTO1.setProductCode("AUDI CAR DUMMY");
+            return orderItemDTO1;
+        }
+
+        @Override
+        public OrderItemDTO updateOrderItem(OrderItemDTO orderItemDTO, Long id) {
+            OrderItemDTO orderItemDTO1=new OrderItemDTO();
+            orderItemDTO1.setId(id);
+            orderItemDTO1.setQuantity("Good");
+            orderItemDTO1.setProductName("AUDI");
+            orderItemDTO1.setProductCode("AUDI CAR DUMMY");
+            return orderItemDTO1;
+        }
+
+        @Override
+        public OrderItemDTO getOrderItemById(Long id) {
+            OrderItemDTO orderItemDTO1=new OrderItemDTO();
+            orderItemDTO1.setId(id);
+            orderItemDTO1.setQuantity("Good");
+            orderItemDTO1.setProductName("AUDI");
+            orderItemDTO1.setProductCode("AUDI CAR DUMMY");
+            return orderItemDTO1;
+        }
+
+        @Override
+        public List<OrderItemDTO> getAllOrderItem() {
+            return null;
+        }
+
+        @Override
+        public void deleteOrderItemById(Long id) {
+
+        }
+    }
 
     }
 
